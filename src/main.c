a277a19a70438110c24cc23c39471b784f6e8cac
Jose Santano <jose.santano@microsoft.com>
2018-06-23 22:27:32 +0100

    Indent with tabs, because we're that strong oppinionated

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-	printf("NESulator");	
+	printf("NESulator");
 	return 0;
 }
--- test/TestingTests.c
+++ test/TestingTests.c
@@ -1,5 +1,5 @@
 #include <assert.h>
 
 int main(){
-    assert(1 == 0);
+	assert(1 == 0);
 }
