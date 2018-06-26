9dd56222c3f68232a65a18d812a97f9a655d3b76
Jose Santano <jose.santano@microsoft.com>
2018-06-26 11:22:41 +0100

    Initial ROM struct creation. we still have to create the ROM array and the CHR array

--- src/rom.c
+++ src/rom.c
@@ -1,5 +1,53 @@
 #include "rom.h"
 
+struct ROM{
+
+	//Header
+	char* nesTitle; //Bytes 0-3 of the ROM
+	byte numROMPages;  //Byte4. Number of 16384 byte program ROM pages. Byte4
+	byte numCHRPages; //Byte5. Number of 8192 byte character ROM pages (0 indicates CHR RAM).
+	/*	NNNN FTBM
+		N: Lower 4 bits of the mapper number
+		F: Four screen mode. 0 = no, 1 = yes. (When set, the M bit has no effect)
+		T: Trainer.  0 = no trainer present, 1 = 512 byte trainer at 7000-71FFh
+		B: SRAM at 6000-7FFFh battery backed.  0= no, 1 = yes
+		M: Mirroring.  0 = horizontal, 1 = vertical.
+	 */
+	byte flags6; //byte 6
+
+	/*
+		NNNN xxPV
+
+		N: Upper 4 bits of the mapper number
+		P: Playchoice 10.  When set, this is a PC-10 game
+		V: Vs. Unisystem.  When set, this is a Vs. game
+		x: these bits are not used in iNES.
+	 */
+	byte flags7; //byte 7
+	byte endOfHeader[8]; //bytes 8-15. These bytes are not used, and should be 00h.
+
+	//ROM
+
+	//CHR
+};
+
+
 void loadROM(char *filePath) {
+	//Open the file
+	//Figure out the total size of the file
+	//Allocate memory for a byte[] of that same size
+	//Go back to the init of the file
+	//byte copy the file to the array
+
+	FILE *file;
+	file = fopen(filePath, "r");
+	fseek(file, 0L, SEEK_END);//Go to the end of the file
+	long fSize = ftell(file);//get the size
+	rewind(file);//Get back to the beginning.
+}
+
+
+
+void printHeaderDebugInfo(byte *fileData[]) {
 
 }
--- src/rom.h
+++ src/rom.h
@@ -1,6 +1,14 @@
 #ifndef NESULATOR_ROM_H
 #define NESULATOR_ROM_H
 
+#include <stdio.h>
+#include "nes.h"
+
+/**
+ * Pretty prints the value of the header of the given NES2.0 file format
+ */
+void printHeaderDebugInfo(byte *fileData[]);
+
 /**
  * Loads a ROM from the specified filePath. What it does with it is still to be determined
  */
