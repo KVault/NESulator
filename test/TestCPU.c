917194a20764fe244723da7b0026ced991d72dcc
Jose Santano <jose.santano@microsoft.com>
2018-07-01 21:05:40 +0100

    Hey! I fixed it

--- test/TestCPU.c
+++ test/TestCPU.c
@@ -13,10 +13,10 @@ void testOpcodes(){
  */
 void test_BRK(){
 	int cachedPC = PC;
-	//assert(bit_test(P,6) == 0);
+	assert(bit_test(P,6) == 0);
 	wmem(BYTE, PC, 0x00);
-	//cpu_cycle();
+	cpu_cycle();
 	assert(cachedPC+1 == PC);
-	//assert(bit_test(P,6) == 1);
-	//assert(bit_test(P,6) == 1);
+	assert(bit_test(P,6) == 1);
+	assert(bit_test(P,6) == 1);
 }
--- test/TestMain.c
+++ test/TestMain.c
@@ -1,9 +1,31 @@
 #include <assert.h>
+#include "../src/Utils.h"
 #include "TestMemory.h"
 #include "TestCPU.h"
 
 int main(){
+	testUtils();
 	testWrite();
 	testRead();
 	testOpcodes();
+}
+
+/**
+ * Tests the utils, we'll put this here for now
+ */
+void testUtils();
+
+void testUtils(){
+	byte testVal = 0b00000000;
+	assert(bit_test(testVal, 0) == 0);
+	assert(bit_test(testVal, 2) == 0);
+	assert(bit_test(testVal, 3) == 0);
+	assert(bit_test(testVal, 5) == 0);
+	assert(bit_test(testVal, 7) == 0);
+	testVal = 0b10100100;
+	assert(bit_test(testVal, 0) == 0);
+	assert(bit_test(testVal, 2) == 1);
+	assert(bit_test(testVal, 3) == 0);
+	assert(bit_test(testVal, 5) == 1);
+	assert(bit_test(testVal, 7) == 1);
 }
