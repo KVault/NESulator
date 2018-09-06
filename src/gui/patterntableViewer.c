f115ec1d5270e5aaede48ba835e739c0bd8674fe
Santanor <josementi@gmail.com>
2018-09-06 18:18:20 +0100

    No colors or anything. But we can see something on the pattern table window

--- src/gui/patterntableViewer.c
+++ src/gui/patterntableViewer.c
@@ -3,17 +3,7 @@
 struct patterntable_viewer_window patterntable_window;
 struct tile *patterns;
 double refresh_time;
-uint back_buffer[PATTERNTABLE_TEXTURE_WIDTH * PATTERNTABLE_TEXTURE_HEIGHT];
-
-int paint_mouse(SDL_Event event){
-	if (event.window.windowID == SDL_GetWindowID(patterntable_window.window)) {
-		int mouseX = event.motion.x;
-		int mouseY = event.motion.y;
-		back_buffer[mouseY * PATTERNTABLE_TEXTURE_WIDTH + mouseX] = 0;
-		return 1;
-	}
-	return 0;
-}
+uint back_buffer[PATTERNTABLE_TEXTURE_WIDTH][PATTERNTABLE_TEXTURE_HEIGHT];
 
 int build_patterntable_viewer(int refresh_rate) {
 	refresh_time = (1.0 / refresh_rate) * MILLISECOND;
@@ -32,11 +22,10 @@ int build_patterntable_viewer(int refresh_rate) {
 	                                                        SDL_TEXTUREACCESS_STATIC, PATTERNTABLE_TEXTURE_WIDTH,
 	                                                         PATTERNTABLE_TEXTURE_HEIGHT);
 
-	memset(back_buffer, 255 , PATTERNTABLE_TEXTURE_WIDTH * PATTERNTABLE_TEXTURE_HEIGHT * sizeof(uint));
+	memset(back_buffer, 0 , PATTERNTABLE_TEXTURE_WIDTH * PATTERNTABLE_TEXTURE_HEIGHT * sizeof(uint));
 	register_window_cycle(&cycle_patterntable_viewer);
 
 	sevent(SDL_WINDOWEVENT, SDL_WINDOWEVENT_CLOSE, &on_quit_patterntable_viewer_window);
-	sevent(SDL_MOUSEMOTION, SDL_MOUSEMOTION, &paint_mouse);
 }
 
 int on_quit_patterntable_viewer_window(SDL_Event event) {
@@ -53,11 +42,23 @@ int cycle_patterntable_viewer() {
 	if((time_aux = has_time_elapsed(last_check, refresh_time))) {
 		last_check = time_aux;
 
-		//free(patterns);
-		//patterns = encode_as_tiles(vram_bank, 0xFF);
+		free(patterns);
+		patterns = encode_as_tiles(vram_bank, 0xFF);
 
 		//IS THIS MAGIC? WTF is this shit
-		//memset(&back_buffer, 255 , PATTERNTABLE_TEXTURE_WIDTH * PATTERNTABLE_TEXTURE_HEIGHT * sizeof(uint));
+		for (byte i = 0; i < 0xFF; ++i) {
+			for (int j = 0; j < 8; ++j) {
+				for (int k = 0; k < 8; ++k) {
+					byte columnIndex = (byte)(i & 0x0F);
+					byte rowIndex = i >> 4;
+					if(patterns[i].pattern[j][k] != 0){
+						back_buffer[(columnIndex * 8) + j][(rowIndex * 8)+ k] = 255; //COLOUR_PALETTE[patterns[i].pattern[j][k] + 0x05];
+					}
+				}
+			}
+		}
+		
+
 		SDL_UpdateTexture(patterntable_window.back_buffer_tex, NULL, back_buffer, PATTERNTABLE_TEXTURE_WIDTH * sizeof(uint));
 
 		SDL_RenderClear(patterntable_window.renderer);
