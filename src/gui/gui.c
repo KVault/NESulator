f1b4e5c650c9556bb1c317b0fe54558c9b3214a5
Santanor <josementi@gmail.com>
2018-11-01 08:35:32 +0000

    Merge branch 'IMGUI' into AvaloniaUI
    # Conflicts:
    #	src/gui/Lua_wrapper/lua_gui_main.c

---
+++ src/gui/gui.c
@@ -0,0 +1,6 @@
+#include "gui.h"
+
+#undef main
+
+int main(){
+}
--- src/gui/gui.h
+++ src/gui/gui.h
@@ -1,13 +1,6 @@
 #ifndef NESULATOR_GUI_H
 #define NESULATOR_GUI_H
 
-#include "imgui/imgui.h"
-#include "SDL2/SDL.h"
-#include "imgui/imgui_impl_sdl.h"
-#include "imgui/imgui_impl_opengl2.h"
-#include "SDL2/SDL_opengl.h"
-#include "GL/glu.h"
-
 int main();
 
 #endif //NESULATOR_GUI_H
